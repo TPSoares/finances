f6cc0cd2dab2c76b1718c3b080a622448786d605
TPSoares <thiagopina.soares@gmail.com>
2019-01-11 11:14:37 -0300

    fixed sign in and sign up forms to required

--- views/home.php
+++ views/home.php
@@ -17,12 +17,12 @@
             <form method="POST" action="<?php echo BASE_URL; ?>dashboard">
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required="true">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="senha" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input type="password" name="senha" class="form-control" id="exampleInputPassword1" placeholder="Password" required="true">
             </div>
             <button type="submit" class="btn btn-primary">Sign in</button>
             <a href="<?php echo BASE_URL; ?>signup" class="btn btn-secondary">Sign up</a>
--- views/signup.php
+++ views/signup.php
@@ -4,11 +4,11 @@
             <form method="POST" action="<?php echo BASE_URL; ?>signup/create">
             <div class="form-group">
                 <label for="exampleInputEmail1">Name</label>
-                <input type="text" name="nome" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+                <input type="text" name="nome" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name" required="true">
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required="true">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">

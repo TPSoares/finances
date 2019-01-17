05b9798f3ba4d6a319e89148079c4c1bb8c618e1
TPSoares <thiagopina.soares@gmail.com>
2019-01-16 23:14:44 -0300

    teste

--- views/template.php
+++ views/template.php
@@ -17,7 +17,7 @@
 
 </head>
 <body onload="teste()">
-    <!-- <h1>Template</h1> -->
+    <!-- <h1>Template</h1> teste -->
     <?php $this->loadViewInTemplate($viewName, $viewData) ?>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
